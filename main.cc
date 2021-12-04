0580996e77b8e472391f28fa9c00f72b84687305
Angel Choquehuanca Anconeyra <84652181+xforenur@users.noreply.github.com>
2021-12-04 01:09:30 -0500

    1

--- main.cc
+++ main.cc
@@ -2,7 +2,12 @@
 #include<vector>
 #include "Grafo.h"
 using namespace std;
-
+string vacio = "vacio.dot";
+string insertarA = "insertarA.dot";
+string insertarN = "insertarN.dot";
+string borrarA = "borrarA.dot";
+string borrarN = "borrarN.dot";
+string dibujo = "dibujo.dot";
 
 
 int main(){
@@ -13,13 +18,19 @@ int main(){
     G.insertarNodo(6);
     G.insertarNodo(8);
     G.insertarNodo(7);
-    G.insertarArista(0,5,7);
+    //Insertar aristas, solo 1 o 0
+    G.insertarArista(1,5,7);
     G.insertarArista(0,7,8);
-    G.insertarArista(0,5,6);
+    G.insertarArista(1,5,6);
     G.insertarArista(0,8,6);
+    G.graficar(insertarA);
+    system("dot -Tjpg -O insertarA.dot");
+    system("insertarA.dot.jpg");
    //G.borrarArista(5,7);
-    G.borrarNodo(5);
-    G.print();
+    //G.borrarNodo(5);
+    //cout<<G.esVacio()<<endl;
+    //cout<<G.sonAdyacentes(8,7)<<endl;
+    G.verAdyacencias();
 
     return 1;
 }
