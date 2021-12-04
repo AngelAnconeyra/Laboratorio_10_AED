9a9f7640f235ff2a47fce7087d8a243556360bfe
Angel Choquehuanca Anconeyra <84652181+xforenur@users.noreply.github.com>
2021-12-04 01:38:11 -0500

    2

--- Grafo.h
+++ Grafo.h
@@ -1,24 +1,26 @@
 #ifndef _GRAFO_H_
 #define _GRAFO_H_
 #include <bits/stdc++.h>
+#include<fstream>
 using namespace std;
 template<class T>
 class Grafo{
     private:
         int nvertices=0;
         vector<list<T>> vertices;
+        vector<list<T>> verticesad;
     public:
         void crearGrafo();
         void insertarNodo(T dato);
         void insertarArista(T valor, T n1, T n2);
+        void insertarAristaAd(T valor, T n1, T n2);
         void borrarNodo(T dato);
         void borrarArista(T n1, T n2);
         bool esVacio();
         bool existeNodo(T);
-        bool sonAdyacentes();
+        bool sonAdyacentes(T,T);
         void verAdyacencias();
-        void graficar();
-        void print();
+        void graficar(string file);
 };
 
 
@@ -32,21 +34,12 @@ void Grafo<T>::crearGrafo(){
 }
 
 template<class T>
-bool Grafo<T>::existeNodo(T dat){
-    for(list<T> a : vertices){
-        if((*a.begin()) == dat)
-            return true;
-    }
-    return false;
-}
-
-
-template<class T>
 void Grafo<T>::insertarNodo(T dato){
     list<T> aux;
     aux.push_back(dato);
     if(existeNodo(dato)==false){
         vertices.push_back(aux);
+        verticesad.push_back(aux);
         nvertices++;    
     }
     return;
@@ -55,10 +48,30 @@ void Grafo<T>::insertarNodo(T dato){
 
 template<class T>
 void Grafo<T>::insertarArista(T valor, T n1, T n2){
+    insertarAristaAd(valor,n1,n2);
+    if(valor == 0) return;
     for(list<T> &a : vertices){
         if((*a.begin()) == n1){
             a.push_back(n2);
             return;      /*Se inserta a la lista*/
+        }
+    }
+}
+
+template<class T>
+void Grafo<T>::insertarAristaAd(T valor, T n1, T n2){
+    if(valor == 0) return;
+    for(list<T> &a : verticesad){
+        if((*a.begin()) == n1){
+            a.push_back(n2);
+            break;      /*Se inserta a la lista*/
+        }
+    }
+
+    for(list<T> &a : verticesad){
+        if((*a.begin()) == n2){
+            a.push_back(n1);      /*Se inserta a la lista*/
+            return;
         }
     }
 }
@@ -84,19 +97,71 @@ void Grafo<T>::borrarArista(T n1, T n2){
     for(list<T> &a : vertices){
         if((*a.begin()) == n1){
             a.remove(n2);
+            break;
+        }
+    }
+    for(list<T> a : vertices){
+        if((*a.begin()) == n2){
+            a.remove(n1);
             return;
         }
     }
 }
 
+template<class T>
+bool Grafo<T>::esVacio(){
+    if(nvertices == 0)return true;
+    return false;
+}
 
 template<class T>
-void Grafo<T>::print(){
+bool Grafo<T>::existeNodo(T dat){
     for(list<T> a : vertices){
+        if((*a.begin()) == dat)
+            return true;
+    }
+    return false;
+}
+
+template<class T>
+bool Grafo<T>::sonAdyacentes(T n1, T n2){
+    for(list<T> a : vertices){
+        if((*a.begin()) == n1){
+            for(T nod : a){
+                if(nod == n2) 
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+template<class T>
+void Grafo<T>::verAdyacencias(){
+    sort(verticesad.begin(), verticesad.end());
+    for(list<T> a : verticesad){
         for(T nod : a ){
             cout << "[" << nod <<"]->";
         }
         cout << "\n";
     }
 }
+
+template<class T>
+void Grafo<T>::graficar(string file){
+    ofstream archivo;
+    archivo.open(file.c_str(),ios::out);
+    archivo << "graph {\n";
+    sort(vertices.begin(), vertices.end());
+    for(list<T> a : vertices){
+        for(T nod : a ){
+            if(*a.begin() != nod){
+                archivo << *a.begin() <<"--"<< nod <<";\n"; 
+            }
+        }
+    }
+    archivo << "}\n";
+    archivo.close();
+}
+
 #endif
