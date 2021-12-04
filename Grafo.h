ff97ac0bd2e74214ab80ee61a5b9e1745ab6cac6
SamuelChambiYtusaca <82793718+SamuelChambiYtusaca@users.noreply.github.com>
2021-12-03 23:46:07 -0500

    :alien:

--- Grafo.h
+++ Grafo.h
@@ -1,13 +1,12 @@
-#ifndef GRAFO_H
-#define GRAFO_H
+#ifndef _GRAFO_H_
+#define _GRAFO_H_
 #include <bits/stdc++.h>
-#include "Node.h"
 using namespace std;
 template<class T>
 class Grafo{
     private:
         int nvertices=0;
-        vector<list<Node<T>>> vertices;
+        vector<list<T>> vertices;
     public:
         void crearGrafo();
         void insertarNodo(T dato);
@@ -26,17 +25,26 @@ class Grafo{
 template<class T>
 void Grafo<T>::crearGrafo(){
     nvertices = 0;
-    for(list<Node<T>> &a: vertices){
+    for(list<T> &a: vertices){
         a.clear();
     }
     vertices.clear();
 }
 
 template<class T>
+bool Grafo<T>::existeNodo(T dat){
+    for(list<T> a : vertices){
+        if((*a.begin()) == dat)
+            return true;
+    }
+    return false;
+}
+
+
+template<class T>
 void Grafo<T>::insertarNodo(T dato){
-    Node<T> nuevo(dato,NULL);
-    list<Node<T>> aux;
-    aux.push_back(nuevo);
+    list<T> aux;
+    aux.push_back(dato);
     if(existeNodo(dato)==false){
         vertices.push_back(aux);
         nvertices++;    
@@ -47,18 +55,16 @@ void Grafo<T>::insertarNodo(T dato){
 
 template<class T>
 void Grafo<T>::insertarArista(T valor, T n1, T n2){
-    for(list<Node<T>> &a : vertices){
-        if((*a.begin()).dato == n1){
-            Node<T> conex(n2,valor); //Nuevo nodo conectado con el valor y el peso de la arista/
-            a.push_back(conex);
-            break;      //Se inserta a la lista/
+    for(list<T> &a : vertices){
+        if((*a.begin()) == n1){
+            a.push_back(n2);
+            break;      /*Se inserta a la lista*/
         }
     }
 
-    for(list<Node<T>> &a : vertices){
-        if((*a.begin()).dato == n2){
-            Node<T> conex(n1,valor); //Nuevo nodo conectado con el valor y el peso de la arista/
-            a.push_back(conex);      //Se inserta a la lista/
+    for(list<T> &a : vertices){
+        if((*a.begin()) == n2){
+            a.push_back(n1);      /*Se inserta a la lista*/
             return;
         }
     }
@@ -66,17 +72,15 @@ void Grafo<T>::insertarArista(T valor, T n1, T n2){
 
 template<class T>
 void Grafo<T>::borrarNodo(T dato){
-    for (auto i = vertices.begin(); i != vertices.end(); i++){
-        if( ( *((*i).begin())).dato == dato ){
-            vertices.erase(i);
-            nvertices--;
-        }
+    for(list<T> &a: vertices){
+        if((*a.begin())==dato) continue;
+        a.remove(dato);
     }
-    for(list<Node<T>> a: vertices){
-        for(auto i = a.begin(); i!=a.end();++i){
-            if((*i).dato == dato){
-                a.erase(i);
-            }
+    for (typename vector<list<T>>::iterator i = vertices.begin(); i != vertices.end(); i++){
+        if( ( *((*i).begin())) == dato ){
+            vertices.erase((i));
+            nvertices--;
+            break;
         }
     }
     return;
@@ -84,59 +88,26 @@ void Grafo<T>::borrarNodo(T dato){
 
 template<class T>
 void Grafo<T>::borrarArista(T n1, T n2){
-    for(list<Node<T>> a : vertices){
-        if((*a.begin()).dato == n1){
-            for(auto i = a.begin(); i!=a.end();++i){
-                if((*i).dato == n2){
-                    a.erase(i);
-                    break;
-                }
-            }
+    for(list<T> &a : vertices){
+        if((*a.begin()) == n1){
+            a.remove(n2);
             break;
         }
     }
-    for(list<Node<T>> a : vertices){
-        if((*a.begin()).dato == n2){
-            for(auto i = a.begin(); i!=a.end();++i){
-                if((*i).dato == n1){
-                    a.erase(i);
-                    return;
-                }
-            }
+    for(list<T> a : vertices){
+        if((*a.begin()) == n2){
+            a.remove(n1);
+            return;
         }
     }
 }
 
-template<class T>
-bool Grafo<T>::esVacio(){
-
-}
-
-template<class T>
-bool Grafo<T>::existeNodo(T dat){
-    for(list<Node<T>> a : vertices){
-        if((*a.begin()).dato == dat)
-            return true;
-    }
-    return false;
-}
-
-template<class T>
-bool Grafo<T>::sonAdyacentes(){
-    
-}
-
-template<class T>
-void verAdyacencias();
-
-template<class T>
-void graficar();
 
 template<class T>
 void Grafo<T>::print(){
-    for(list<Node<T>>a : vertices){
-        for( Node<T> nod : a ){
-            cout << "[" << nod.dato << ", " << nod.ValArista << "]->";
+    for(list<T> a : vertices){
+        for(T nod : a ){
+            cout << "[" << nod <<"]->";
         }
         cout << "\n";
     }
