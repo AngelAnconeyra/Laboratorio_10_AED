b2c7df758b47eb73ef6bdb5eb99328cf8e0ccd18
SamuelChambiYtusaca <82793718+SamuelChambiYtusaca@users.noreply.github.com>
2021-12-20 19:08:28 -0500

    :alien:

--- main.cc
+++ main.cc
@@ -1,56 +1,25 @@
 #include<iostream>
 #include<vector>
+#include <stdlib.h>
 #include "Grafo.h"
 using namespace std;
-string vacio = "vacio.dot";
-string insertarA = "insertarA.dot";
-string insertarN = "insertarN.dot";
-string borrarA = "borrarA.dot";
-string borrarN = "borrarN.dot";
-string dibujo = "dibujo.dot";
+
 
 
 int main(){
-    Grafo<int> G;
+    Grafo<string,int> G;
     G.crearGrafo();
-    G.graficar(insertarA);
-    system("dot -Tjpg -O vacio.dot");
-    system("vacio.dot.jpg");
-    G.insertarNodo(5);
-    G.insertarNodo(5);
-    G.insertarNodo(6);
-    G.insertarNodo(8);
-    G.insertarNodo(7);
-    G.graficar(insertarN);
-    system("dot -Tjpg -O insertarN.dot");
-    system("insertarN.dot.jpg");
-    system("pause");
-    //Insertar aristas, solo 1 o 0
-    G.insertarArista(1,5,7);
-    G.insertarArista(0,7,8);
-    G.insertarArista(1,5,6);
-    G.insertarArista(0,8,6);
-    G.graficar(insertarA);
-    system("dot -Tjpg -O insertarA.dot");
-    system("insertarA.dot.jpg");
-    system("pause");
-    G.borrarArista(5,7);
-    G.graficar(insertarA);
-    system("dot -Tjpg -O borrarA.dot");
-    system("borrarA.dot.jpg");
-    system("pause");
-    G.borrarNodo(5);
-    G.graficar(borrarN);
-    system("dot -Tjpg -O borrarN.dot");
-    system("borrarN.dot.jpg");
-    system("pause");
-    cout<<G.esVacio()<<endl;
-    cout<<G.sonAdyacentes(8,7)<<endl;
-    cout<<"\nLista de adyacencia:\n";
-    G.verAdyacencias();
-    G.graficar(dibujo);
-    system("dot -Tjpg -O dibujo.dot");
-    system("dibujo.dot.jpg");
-    system("pause");
+    G.insertarNodo("Arequipa");
+    G.insertarNodo("Sullana");
+    G.insertarNodo("Lima");
+    G.insertarNodo("Huanuco");
+    G.insertarNodo("Chiclayo");
+    G.insertarArista(1881,"Chiclayo","Arequipa");
+    G.insertarArista(310,"Sullana","Arequipa");
+    G.insertarArista(8000,"Lima","Huanuco");
+    G.print();
+   // G.borrarNodo("Arequipa");
+   // G.print();
+
     return 1;
 }
